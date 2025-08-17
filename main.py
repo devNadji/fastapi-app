2f16d542e215cc8856109e15c0216c1b170a843a
devNadji <oumtinadji@gmail.com>
2025-08-18 01:05:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI
-import requests
+import requests, random
 
 app = FastAPI()
 
@@ -9,8 +9,13 @@ session = requests.Session()
 session.headers.update({
     "User-Agent": "Instagram 275.0.0.27.98 Android (29/10; 320dpi; 720x1369; Xiaomi; Redmi 8A; olivelite; qcom; en_US; 458229237)"
 })
+f = [
+    "https://www.instagram.com/api/v1/web/accounts/web_create_ajax/attempt/", ###
+    "https://i.instagram.com/api/v1/accounts/create_business/", ####
+    "https://i.instagram.com/api/v1/users/check_username/", ##
+    "https://www.instagram.com/api/v1/users/check_username/" ###
+]
 
-TARGET_URL = "https://i.instagram.com/api/v1/users/check_username/"
 
 @app.get("/")
 async def root():
@@ -22,9 +27,9 @@ def check_username(username: str):
     param: username -> يجي من اليوزر
     """
     payload = {
-        "signed_body": f'SIGNATURE.{{"is_group_creation":"false","username":"{username}","_uuid":"48041674-e663-4131-8f5b-428c4896cbbe"}}'
+        "username": username,
     }
-
+    TARGET_URL = random.choice(f)
     try:
         resp = session.post(TARGET_URL, data=payload)
         return resp.json()  # يرجع JSON مباشر
